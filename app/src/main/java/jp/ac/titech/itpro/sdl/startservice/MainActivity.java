794de321d3d62e543489c60f4421289310b14bcb
shoman <shoma216@gmail.com>
2019-06-13 22:54:16 +0900

    implement service3

--- app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/startservice/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.titech.itpro.sdl.startservice;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,11 +12,15 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = MainActivity.class.getSimpleName();
 
+    private BroadcastReceiver broadcastReceiver;
+    private IntentFilter intentFilter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate in " + Thread.currentThread());
         setContentView(R.layout.activity_main);
+        initBroadcastReceiver();
     }
 
     public void onClickTest1(View v) {
@@ -28,5 +35,39 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Service2.class);
         intent.putExtra(Service2.EXTRA_MYARG, "Hello, Service2");
         startService(intent);
+    }
+
+    public void onClickTest3(View v) {
+        Log.d(TAG, "onClickTest3 in " + Thread.currentThread());
+        Intent intent = new Intent(this, Service3.class);
+        intent.putExtra(Service3.EXTRA_MYARG, "Hello, Service3");
+        startService(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    private void initBroadcastReceiver() {
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                Log.d(TAG, "onReceive: " + action);
+                intent.putExtra(Service3.EXTRA_MYARG, "Hello, Service3");
+            }
+        };
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(Service3.ACTION);
     }
 }
